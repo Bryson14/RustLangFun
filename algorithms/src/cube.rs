c8ba702103e6181dd3b0787dc81066f2181b3aac
Bryson <bryson.meiling@aggiemail.usu.edu>
2021-05-04 23:51:59 -0600

    working on objects

---
+++ algorithms/src/cube.rs
@@ -0,0 +1,15 @@
+pub struct Cube {
+
+    pub fn Cube() {
+        self.h = 1;
+        self.w = 1;
+        self.l = 1;
+    }
+
+    pub fn Cube(h: i32, w: i32, l: i32) {
+        self.h = h;
+        self.w = w;
+        self.l = l;
+    }
+
+}
