55d3e136cac4e7a42fa7ba730c24f9ce1858a517
Bryson <bryson.meiling@gmail.com>
2021-07-04 15:49:02 -0400

    copy and clone traits with Strings are hard

--- blackjack/src/main.rs
+++ blackjack/src/main.rs
@@ -18,12 +18,13 @@ fn main() {
 
     loop{
         players = get_bets(players);
+        let copy_deck = d.clone();
         players = deal_hands(players, d);
         println!("Cards of player 1 {}, {}", players[0].cards[0], players[0].cards[1]);
         show_dealer_hand(&players[players.len()-1], false);
         for i in 0..num_players {
             if players[i as usize].bet > 0 {
-                players = player_turn(i as usize, players);
+                players = player_turn(i as usize, copy_deck, players);
             }
         }
         // self.dealerTurn()
@@ -104,23 +105,22 @@ fn show_player_hand() {
 
 }
 
-fn player_turn(player_idx: usize, mut players: Vec<User>) -> Vec<User> {
+fn player_turn(player_idx: usize, mut deck: Deck, mut players: Vec<User>) -> Vec<User> {
     loop{
         show_player_hand();
         if players[player_idx].status != -1 {
             let res = get_int_input(format!("Player {}: Hit (1) or Hold (2)? ", player_idx+1));
             if res == 1 {
                 //hit
-                //so cool
-                // let card: Card = deck.draw();
-                // players[player_idx].cards.push(card);
-                // let bust_sum = 0;
-                // for c in players[player_idx].cards.iter() {
-                //     bust_sum += c.lower_val;
-                // }
-                // if bust_sum > 21 {
-                //     players[player_idx].status = -1;
-                // }
+                let card: Card = deck.draw();
+                players[player_idx].cards.push(card);
+                let bust_sum = 0;
+                for c in players[player_idx].cards.iter() {
+                    bust_sum += c.lower_val;
+                }
+                if bust_sum > 21 {
+                    players[player_idx].status = -1;
+                }
             } else if res == 2 {
                 println!("Player {} Holds\n <<====>>", player_idx+1);
                 break;
