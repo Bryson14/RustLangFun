899175e61d1c7291c62243701c85be41f993ad42
Bryson <bryson.meiling@gmail.com>
2021-10-21 10:38:32 -0400

    made aanother stateup system

--- bevy-snake-game/src/main.rs
+++ bevy-snake-game/src/main.rs
@@ -13,6 +13,16 @@ const MAX_ENEMIES: u32 = 6;
 const MAX_FORMATION_MEMBERS: u32 = 2;
 const PLAYER_RESPAWN_DELAY: f64 = 1.5;
 
+// resources
+pub struct Materials {
+    player_materials: Handle<ColorMaterial>,
+}
+
+struct WinSize {
+    w: f32,
+    h: f32,
+}
+
 fn main() {
     App::build()
         .insert_resource(ClearColor(Color::rgb(0.1, 0.1, 0.1)))
@@ -24,6 +34,10 @@ fn main() {
         })
         .add_plugins(DefaultPlugins)
         .add_startup_system(setup.system())
+        .add_startup_stage(
+            "game_setup_actors",
+            SystemStage::single(player_spawn.system()),
+        )
         .run();
 }
 
@@ -37,13 +51,33 @@ fn setup(
     commands.spawn_bundle(OrthographicCameraBundle::new_2d());
 
     // position window
-    // let mut window = windows.get_primary_mut().unwrap();
+    let mut window = windows.get_primary_mut().unwrap();
     // window.set_position(IVec2::new(3870, 4830));
 
-    // spawn main character big rectangle
+    // insert window size into a resource
+    commands.insert_resource(WinSize {
+        w: window.width(),
+        h: window.height(),
+    });
+
+    // creates the resource once and loads it into bevy engine
+    // better for performance
+    commands.insert_resource(Materials {
+        player_materials: materials.add(asset_server.load(PLAYER_SPRITE).into()),
+    })
+}
+
+fn player_spawn(mut commands: Commands, materials: Res<Materials>, win_size: Res<WinSize>) {
+    // spawn main character
+    let bottom = -win_size.h / 2.;
     commands.spawn_bundle(SpriteBundle {
-        material: materials.add(asset_server.load(PLAYER_SPRITE).into()),
-        sprite: Sprite::new(Vec2::new(200.0, 100.)),
+        material: materials.player_materials.clone(),
+        transform: Transform {
+            // bottom plus half the height of ferris plus some padding
+            translation: Vec3::new(0., bottom + 75. / 4. + 5., 10.),
+            scale: Vec3::new(0.5, 0.5, 1.),
+            ..Default::default()
+        },
         ..Default::default()
     });
 }
