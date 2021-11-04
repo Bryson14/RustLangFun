d4393d70f52b083b7f5dc37f9897c34e516e34b3
Bryson <bryson.meiling@gmail.com>
2021-11-04 10:03:29 -0400

    understanding mutability and pointers

--- http-server/src/main.rs
+++ http-server/src/main.rs
@@ -1,3 +1,30 @@
-fn main() {
-    println!("Hello, world!");
+use error_chain::error_chain;
+use reqwest;
+use std::io::Read;
+
+error_chain! {
+    foreign_links {
+        Io(std::io::Error);
+        HttpRequest(reqwest::Error);
+    }
 }
+
+fn main() -> Result<()> {
+    let mut res = reqwest::blocking::get("http://reddit.com/")?;
+    let mut body = String::new();
+    res.read_to_string(&mut body)?;
+
+    // println!("Status: {}", res.status());
+    // println!("Headers:\n{:#?}", res.headers());
+    // println!("Body:\n{}", body);
+
+    let mut a: [i32; 5] = [10, 20, 30, 40, 50];
+    println!("a before: {:?}", a);
+    move_int(&mut a);
+
+    println!("a after: {:?}", a);
+
+    Ok(())
+}
+
+fn move_int(i: &mut [i32; 5]) {}
