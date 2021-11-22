8b23a6ec3bf8a3ce546025052141b7479fd7ee61
Bryson <bryson.meiling@gmail.com>
2021-11-21 23:35:44 -0500

    port 8080

--- http-server/src/main.rs
+++ http-server/src/main.rs
@@ -3,7 +3,7 @@ use std::net::TcpListener;
 use std::net::TcpStream;
 
 fn main() {
-    let port = "127.0.0.1:80";
+    let port = "127.0.0.1:8080";
     let listener = TcpListener::bind(port).expect("Error binding to the port");
 
     for stream in listener.incoming() {
