95d102632ed0a3b7bb5ba526b203bca0edc3240d
Bryson <bryson.meiling@gmail.com>
2021-11-23 12:52:32 -0500

    paralellism only works for expensive operations

--- stonks/src/main.rs
+++ stonks/src/main.rs
@@ -1,7 +1,31 @@
+extern crate rayon;
 use rayon::prelude::*;
+use std::thread::sleep;
+use std::time::{Duration, Instant};
+
+fn sqrts(floats: Vec<f64>) -> Vec<f64> {
+    floats.par_iter().map(|&f| expensive_operation(f)).collect()
+}
+
+fn sqrts_seq(floats: Vec<f64>) -> Vec<f64> {
+    floats.iter().map(|&f| expensive_operation(f)).collect()
+}
+
+fn expensive_operation(time: f64) -> f64 {
+    sleep(Duration::from_millis(time as u64));
+    (time * time).into()
+}
 
 fn main() {
-    println!("please work");
+    let v: Vec<f64> = (0..100).map(f64::from).collect();
+    let v2 = v.clone();
 
-    let names = ["bryson", "james", "lenovo", "ip", "something"];
+    let now = Instant::now();
+    sqrts(v);
+    let parallel = now.elapsed().as_millis();
+
+    let now = Instant::now();
+    sqrts_seq(v2);
+    let seq = now.elapsed().as_millis();
+    println!("parallel {}, seqential {}", parallel, seq);
 }
