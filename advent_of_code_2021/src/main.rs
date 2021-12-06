17b51e72a5bc330dba2392279e2a3037398dbd28
Bryson <bryson.meiling@gmail.com>
2021-12-06 09:02:21 -0500

    solved day 2

--- advent_of_code_2021/src/main.rs
+++ advent_of_code_2021/src/main.rs
@@ -1,5 +1,6 @@
 mod data_tools;
 mod day1;
+mod day2;
 
 use data_tools::{read_from_data_dir, string_to_vec_i32};
 
@@ -7,4 +8,8 @@ fn main() {
     // # Day 1
     day1::part1();
     day1::part2();
+
+    // # Day 2
+    day2::part1();
+    day2::part2();
 }
