12bb584b7d87c0a427666cc577f567d295513c7c
Bryson <bryson.meiling@gmail.com>
2022-06-13 13:08:59 -0400

    working on wasm-pack and bindgen. also the random module conversion is a pain

--- minesweeper_wasm/src/main.rs
+++ minesweeper_wasm/src/main.rs
@@ -3,11 +3,8 @@ mod lib;
 use lib::MineSweeper;
 
 fn main() {
-    let mut ms = MineSweeper::new(5, 5, 3);
-    for x in 0..5 {
-        for y in 0..5 {
-            println!("Clicked {},{}. Keep playing? {}", x, y, ms.click(x, y));
-            println!("board: {}", ms);
-        }
-    }
+    let mut ms = MineSweeper::new(10, 10, 16);
+    ms.place_flag(3, 3);
+    ms.click(4, 3);
+    println!("{ms}");
 }
