86981d939bce5659f67f398fe76f73fe14009952
Bryson <bryson.meiling@gmail.com>
2022-08-15 09:51:03 -0400

    nice work dude!

--- Todo-List-Server/src/main.rs
+++ Todo-List-Server/src/main.rs
@@ -1,11 +1,27 @@
-use extern crate rocket;
+#[macro_use]
+extern crate rocket;
+use rocket::tokio::time::{sleep, Duration};
 
 #[get("/")]
 fn index() -> &'static str {
     "Hello, World!"
 }
 
+#[get("/about")]
+fn about() -> &'static str {
+    "check out this website!"
+}
+
+#[get("/delay/<seconds>")]
+async fn delay(seconds: u64) -> String {
+    sleep(Duration::from_secs(seconds)).await;
+    format!("I slept for {seconds} seconds!")
+}
+
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/", routes![index])
+    rocket::build()
+        .mount("/", routes![index])
+        .mount("/", routes![about])
+        .mount("/", routes![delay])
 }
