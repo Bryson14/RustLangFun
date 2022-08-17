b6e4c8f3759b0e09197435bb9279c254eda1cfe5
Bryson <bryson.meiling@gmail.com>
2022-08-17 10:13:22 -0400

    working on understanding the deisel dtatbase

--- Todo-List-Server/src/main.rs
+++ Todo-List-Server/src/main.rs
@@ -1,7 +1,6 @@
 #[macro_use]
 extern crate rocket;
 use rocket::fs::{relative, FileServer};
-use rocket::response::content::Html;
 use rocket::tokio::time::{sleep, Duration};
 use rocket_db_pools::sqlx::{self, Row};
 use rocket_db_pools::{Connection, Database};
@@ -16,7 +15,7 @@ fn index() -> &'static str {
 struct Todos(sqlx::SqlitePool);
 
 #[get("/<id>")]
-async fn read(mut db: Connection<Logs>, id: i64) -> Option<String> {
+async fn read(mut db: Connection<Todos>, id: i64) -> Option<String> {
     sqlx::query("SELECT content FROM Todos WHERE id = ?")
         .bind(id)
         .fetch_one(&mut *db)
@@ -25,12 +24,9 @@ async fn read(mut db: Connection<Logs>, id: i64) -> Option<String> {
         .ok()
 }
 
-#[post("/todo")]
 #[launch]
 fn rocket() -> _ {
     rocket::build()
         .mount("/", routes![index])
-        .mount("/", routes![about])
-        .mount("/", routes![delay])
         .mount("/public", FileServer::from(relative!("static/")))
 }
