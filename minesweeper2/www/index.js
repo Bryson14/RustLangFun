75ce956a4bf50b27cd80bf861ac3b5941a412e31
Bryson <bryson.meiling@gmail.com>
2022-08-30 08:20:52 -0400

    js stuff

--- minesweeper2/www/index.js
+++ minesweeper2/www/index.js
@@ -7,177 +7,43 @@ const DEAD_COLOR = "#FFFFFF";
 const ALIVE_COLOR = "#000000";
 
 // Construct the game, and get its width and height.
-const width = 2;
-const height = 2;
-const bombs = 2;
-const game = MineSweeper.new(width, height, bombs);
-const cellsPtr = game.game_state;
-const cells = new Uint8Array(memory.buffer, cellsPtr, width * height);
+
 console.log("ok", cells);
 
-// Give the canvas room for all of our cells and a 1px border
-// around each of them.
-const canvas = document.getElementById("minesweeper-canvas");
-canvas.height = (CELL_SIZE + 1) * height + 1;
-canvas.width = (CELL_SIZE + 1) * width + 1;
+let playing = false;
 
-const ctx = canvas.getContext("2d");
+const resetBoard = () => {};
 
-const fps = new (class {
-  constructor() {
-    this.fps = document.getElementById("fps");
-    this.frames = [];
-    this.lastFrameTimeStamp = performance.now();
-  }
+const setupBoard = () => {
+  const width = Number(document.getElementById("size-input").innerText);
+  const height = Number(document.getElementById("size-input").innerText);
+  const mines = Number(document.getElementById("mines-input").innerText);
+  const game = MineSweeper.new(width, height, bombs);
 
-  render() {
-    // Convert the delta time since the last frame render into a measure
-    // of frames per second.
-    const now = performance.now();
-    const delta = now - this.lastFrameTimeStamp;
-    this.lastFrameTimeStamp = now;
-    const fps = (1 / delta) * 1000;
-
-    // Save only the latest 100 timings.
-    this.frames.push(fps);
-    if (this.frames.length > 100) {
-      this.frames.shift();
-    }
-
-    // Find the max, min, and mean of our 100 latest timings.
-    let min = Infinity;
-    let max = -Infinity;
-    let sum = 0;
-    for (let i = 0; i < this.frames.length; i++) {
-      sum += this.frames[i];
-      min = Math.min(this.frames[i], min);
-      max = Math.max(this.frames[i], max);
-    }
-    let mean = sum / this.frames.length;
-
-    // Render the statistics.
-    this.fps.textContent = `
-Frames per Second:
-         latest = ${Math.round(fps)}
-avg of last 100 = ${Math.round(mean)}
-min of last 100 = ${Math.round(min)}
-max of last 100 = ${Math.round(max)}
-`.trim();
-  }
-})();
-
-const renderLoop = () => {
-  fps.render();
-
-  drawGrid();
-  drawCells();
-
-  for (let i = 0; i < 9; i++) {
-    // universe.tick();
-  }
-
-  animationId = requestAnimationFrame(renderLoop);
-};
-
-const isPaused = () => {
-  return animationId === null;
-};
-
-const playPauseButton = document.getElementById("play-pause");
-
-playPauseButton.addEventListener("click", (event) => {
-  if (isPaused()) {
-    play();
-  } else {
-    pause();
-  }
-});
-
-const drawGrid = () => {
-  ctx.beginPath();
-  ctx.strokeStyle = GRID_COLOR;
-
-  // Vertical lines.
-  for (let i = 0; i <= width; i++) {
-    ctx.moveTo(i * (CELL_SIZE + 1) + 1, 0);
-    ctx.lineTo(i * (CELL_SIZE + 1) + 1, (CELL_SIZE + 1) * height + 1);
-  }
-
-  // Horizontal lines.
-  for (let j = 0; j <= height; j++) {
-    ctx.moveTo(0, j * (CELL_SIZE + 1) + 1);
-    ctx.lineTo((CELL_SIZE + 1) * width + 1, j * (CELL_SIZE + 1) + 1);
-  }
-
-  ctx.stroke();
-};
-
-const getIndex = (row, column) => {
-  return row * width + column;
-};
-
-const drawCells = () => {
   const cellsPtr = game.game_state;
   const cells = new Uint8Array(memory.buffer, cellsPtr, width * height);
 
-  ctx.beginPath();
-
-  // Alive cells.
-  ctx.fillStyle = ALIVE_COLOR;
-  for (let row = 0; row < height; row++) {
-    for (let col = 0; col < width; col++) {
-      const idx = getIndex(row, col);
-      if (cells[idx] !== Cell.Alive) {
-        continue;
-      }
-
-      ctx.fillRect(
-        col * (CELL_SIZE + 1) + 1,
-        row * (CELL_SIZE + 1) + 1,
-        CELL_SIZE,
-        CELL_SIZE
-      );
-    }
+  if (width > 100 || height > 100) {
+    alert("bad game dimension");
+    return;
   }
 
-  // Dead cells.
-  ctx.fillStyle = DEAD_COLOR;
-  for (let row = 0; row < height; row++) {
-    for (let col = 0; col < width; col++) {
-      const idx = getIndex(row, col);
-      if (cells[idx] !== Cell.Dead) {
-        continue;
-      }
-
-      ctx.fillRect(
-        col * (CELL_SIZE + 1) + 1,
-        row * (CELL_SIZE + 1) + 1,
-        CELL_SIZE,
-        CELL_SIZE
-      );
-    }
+  if (mines > width * height) {
+    alert("too many mines");
+    return;
   }
-
-  ctx.stroke();
 };
 
-canvas.addEventListener("click", (event) => {
-  const boundingRect = canvas.getBoundingClientRect();
+const gameGrid = document.getElementById("game-grid");
 
-  const scaleX = canvas.width / boundingRect.width;
-  const scaleY = canvas.height / boundingRect.height;
-
-  const canvasLeft = (event.clientX - boundingRect.left) * scaleX;
-  const canvasTop = (event.clientY - boundingRect.top) * scaleY;
-
-  const row = Math.min(Math.floor(canvasTop / (CELL_SIZE + 1)), height - 1);
-  const col = Math.min(Math.floor(canvasLeft / (CELL_SIZE + 1)), width - 1);
-
-  universe.toggle_cell(row, col);
-
-  drawCells();
-  drawGrid();
+const playResetButton = document.getElementById("play-reset");
+playResetButton.addEventListener("click", (event) => {
+  console.log("here");
+  if (playing) {
+    playing = false;
+    resetBoard();
+  } else {
+    playing = true;
+    setupBoard();
+  }
 });
-
-drawCells();
-drawGrid();
