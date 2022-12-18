771333864a6b146d43ad8a62959fe5728b6da0f9
Bryson <bryson.meiling@gmail.com>
2022-12-18 15:59:25 -0500

    cant find issue with find_air_cubes

--- advent_of_code_2022/src/day18.rs
+++ advent_of_code_2022/src/day18.rs
@@ -1,5 +1,6 @@
 #![allow(unused)]
 use crate::utils::read_data;
+use std::{collections::HashSet, future::poll_fn};
 
 const FILE: &str = "day18.txt";
 const DAY: &str = "{{ DAY 18 }}";
@@ -17,6 +18,9 @@ pub fn part1() {
 
 pub fn part2() {
     let data = read_data(FILE);
+    let cube_positions: Vec<Cube> = read_cube_positions(data);
+    let ans = sum_exterior_surface_area(cube_positions);
+    println!("{DAY}-2 Total exterior surface area of droplets is {ans}");
 }
 
 fn read_cube_positions(data: String) -> Vec<Cube> {
@@ -31,14 +35,93 @@ fn read_cube_positions(data: String) -> Vec<Cube> {
         .collect()
 }
 
-fn find_air_cubes(cubes: &Vec<Cube>) -> Vec<Cube> {
-    todo!()
+fn find_air_cubes(cubes: &Vec<Cube>) -> HashSet<Cube> {
+    let mut cubes_set: HashSet<&Cube> = HashSet::from_iter(cubes.iter());
+    let mut possible_pockets = Vec::with_capacity(cubes.len() * 6);
+    for cube in cubes.iter() {
+        let x_plus = Cube {
+            x: cube.x + 1,
+            y: cube.y,
+            z: cube.z,
+        };
+        if !cubes_set.contains(&x_plus) {
+            possible_pockets.push(x_plus);
+        }
+        if cube.x > 0 {
+            let x_minus = Cube {
+                x: cube.x - 1,
+                y: cube.y,
+                z: cube.z,
+            };
+            if !cubes_set.contains(&x_minus) {
+                possible_pockets.push(x_minus);
+            }
+        }
+        let y_plus = Cube {
+            x: cube.x,
+            y: cube.y + 1,
+            z: cube.z,
+        };
+        if !cubes_set.contains(&y_plus) {
+            possible_pockets.push(y_plus);
+        }
+        if cube.y > 0 {
+            let y_minus = Cube {
+                x: cube.x,
+                y: cube.y - 1,
+                z: cube.z,
+            };
+            if !cubes_set.contains(&y_minus) {
+                possible_pockets.push(y_minus);
+            }
+        }
+        let z_plus = Cube {
+            x: cube.x,
+            y: cube.y,
+            z: cube.z + 1,
+        };
+        if !cubes_set.contains(&z_plus) {
+            possible_pockets.push(z_plus);
+        }
+        if cube.z > 0 {
+            let z_minus = Cube {
+                x: cube.x,
+                y: cube.y,
+                z: cube.z - 1,
+            };
+            if !cubes_set.contains(&z_minus) {
+                possible_pockets.push(z_minus);
+            }
+        }
+    }
+
+    let mut i = 0;
+    while i < possible_pockets.len() {
+        let mut neighbors = 0;
+        for cube in cubes {
+            if possible_pockets[i].are_neighbors(cube) {
+                neighbors += 1;
+            }
+        }
+
+        if neighbors == 6 {
+            // is a air pocket
+            i += 1;
+        } else {
+            possible_pockets.remove(i);
+        }
+    }
+    HashSet::from_iter(possible_pockets.into_iter())
 }
 
 fn sum_exterior_surface_area(cubes: Vec<Cube>) -> usize {
     // for each cube, if it has neighbor in one of the 6 directions, then remove a surface area unit
     let mut sum_area = 0;
     let air_pockets = find_air_cubes(&cubes);
+    println!("Pockets");
+    for p in air_pockets.iter() {
+        println!("{p:?}");
+    }
     for i in 0..cubes.len() {
         let mut neighbors = 0;
         let mut pocket_neighbors = 0;
@@ -52,7 +135,7 @@ fn sum_exterior_surface_area(cubes: Vec<Cube>) -> usize {
             }
         }
 
-        // subtract air pockets too
+        // subtract air pockets too from overall surface area
         for pocket in air_pockets.iter() {
             if cubes[i].are_neighbors(pocket) {
                 pocket_neighbors += 1;
@@ -90,6 +173,7 @@ fn sum_surface_area(cubes: Vec<Cube>) -> usize {
     sum_area
 }
 
+#[derive(PartialEq, Eq, Hash, Debug)]
 struct Cube {
     x: usize,
     y: usize,
@@ -133,5 +217,66 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test() {}
+    fn test_find_air_pockets() {
+        let data = "2,2,2
+        1,2,2
+        3,2,2
+        2,1,2
+        2,3,2
+        2,2,1
+        2,2,3
+        2,2,4
+        2,2,6
+        1,2,5
+        3,2,5
+        2,1,5
+        2,3,5";
+        let cubes = read_cube_positions(data.into());
+        let pockets = find_air_cubes(&cubes);
+        let pockets = Vec::from_iter(pockets.into_iter());
+        assert_eq!(pockets[0], Cube { x: 2, y: 2, z: 5 });
+        assert_eq!(pockets.len(), 1);
+        println!("{pockets:?}");
+    }
+
+    #[test]
+    fn test_find_air_pockets_2() {
+        let data = "2,2,2
+        0,2,2
+        1,3,2
+        1,1,2
+        1,2,1
+        1,2,3";
+        /// 1,2,2
+        let cubes = read_cube_positions(data.into());
+        let pockets = find_air_cubes(&cubes);
+        let pockets = Vec::from_iter(pockets.into_iter());
+        assert_eq!(pockets[0], Cube { x: 1, y: 2, z: 2 });
+        assert_eq!(pockets.len(), 1);
+        println!("{pockets:?}");
+    }
+
+    #[test]
+    fn test_find_air_pockets_3() {
+        let data = "2,2,2
+        0,2,2
+        1,3,2
+        1,1,2
+        1,2,1
+        1,2,3
+        2,3,3
+        4,3,3
+        3,4,3
+        3,2,3
+        3,3,2
+        3,3,4";
+        /// 3,3,3
+        let cubes = read_cube_positions(data.into());
+        let pockets = find_air_cubes(&cubes);
+        let pockets = Vec::from_iter(pockets.into_iter());
+        assert_eq!(pockets[1], Cube { x: 3, y: 3, z: 3 });
+        assert_eq!(pockets[0], Cube { x: 1, y: 2, z: 2 });
+        assert_eq!(pockets.len(), 2);
+        println!("{pockets:?}");
+    }
 }
