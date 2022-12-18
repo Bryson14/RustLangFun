bf3865d9ad9a4e2b037667c74b8432fcb8339701
Bryson <bryson.meiling@gmail.com>
2022-12-17 20:27:44 -0500

    testris!

--- advent_of_code_2022/src/day17.rs
+++ advent_of_code_2022/src/day17.rs
@@ -1,15 +1,42 @@
 #![allow(unused)]
+use std::{fs::read, intrinsics::unreachable};
+
 use crate::utils::read_data;
 
-const FILE: &str = "day10.txt";
-const DAY: &str = "{{ DAY 10 }}";
+const FILE: &str = "day17.txt";
+const DAY: &str = "{{ DAY 17 }}";
 
 pub fn part1() {
     let data = read_data(FILE);
+    let jet_blasts = read_air_jets(data);
 }
 
 pub fn part2() {
     let data = read_data(FILE);
+}
+
+enum TetrisPiece {
+    Horizontal,
+    Cross,
+    Elbow,
+    Vertical,
+    Square,
+}
+
+enum AirDirection {
+    Left,
+    Right,
+}
+
+fn read_air_jets(data: String) -> Vec<AirDirection> {
+    data.trim()
+        .chars()
+        .map(|c| match c {
+            '<' => AirDirection::Left,
+            '>' => AirDirection::Right,
+            _ => unreachable!(),
+        })
+        .collect()
 }
 
 #[cfg(test)]
